bb0cb5e8c7bcb8684e19ba24e9dcbb1790cacf03
lanaleyla <lanali999@gmail.com>
2019-06-28 10:43:40 +0300

    exercise 4 js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,7 +2,7 @@ let n=100; //iterate fizzbuzz function untill n value;
 
 function calcFizzBuzz(number)
 {
-    for (let i=0;i<100;i++)
+    for (let i=0;i<number;i++)
     {
     if(i%3===0 && i%5!==0)
         console.log("Fizz");
@@ -14,4 +14,4 @@ function calcFizzBuzz(number)
     }
 }
 
-calcFizzBuzz(n);
+calcFizzBuzz(n); 
